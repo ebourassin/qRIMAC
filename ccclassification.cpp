3dd7c9febdc49ce2f8bf459c80d4e00dc3484e0b
ebourassin <36472260+ebourassin@users.noreply.github.com>
2018-06-14 07:28:56 -1000

    Update ccclassification.cpp

--- ccclassification.cpp
+++ ccclassification.cpp
@@ -11,7 +11,8 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#                    COPYRIGHT: Daniel Girardeau-Montaut                 #
+//#          COPYRIGHT: Emile Blettery, Emmanuel Bourassin                 #
+//#                     Guillemette Fonteix (ENSG)                         #
 //#                                                                        #
 //##########################################################################
 
